746f9321b59c560206fc32f672931c6efa7809f1
Suissa <jnascimento@gmail.com>
2017-06-04 05:08:55 -0300

    12 refatorado mas tem mais

--- aulas/aula01/isPrime12.refatorado.js
+++ aulas/aula01/isPrime12.refatorado.js
@@ -1,19 +1,23 @@
-
+const isNot = ( x ) => !x
+const even = ( num ) => ( num % 2 )
 const halfOf = ( num ) => Math.ceil( num / 2 )
+const disibleBy = ( num ) => ( i ) => isNot( num % i )
 
 const hasDivisor = ( num ) => {
+  const isDisibleBy = disibleBy( num )
   for ( let i = halfOf( num ); i >= 2; i-- ) {
-    if( !( num % i ) ) {
+    if( isDisibleBy( i ) )  {
       return true
     }
   }
   return false
 }
+
 const hasIntegerSquareRoot = ( num ) => 
   ( Number.isInteger( Math.sqrt( num ) ) )
 
 const isEvenAndNotTwo = ( num ) => 
-  ( !( num % 2 ) && ( num !== 2 ) )
+  ( isNot ( even( num ) ) && ( num !== 2 ) )
 
 const isPrime = ( num ) => 
   ( isEvenAndNotTwo( num ) || 
@@ -22,6 +26,8 @@ const isPrime = ( num ) =>
       ? false
       : true
 
+
+console.log('filter', [2, 3, 4, 5].filter(isPrime))
 
 console.log('isPrime 2', isPrime(2))
 console.log('isPrime 3', isPrime(3))
